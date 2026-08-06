18979fba1e479cfac674afed266b8ccedc1c731c
Jack Gravil <jackgravil@sillylittlegames.studio>
2026-08-06 16:40:19 +0100

    Add Licences to all files

--- src/components/heros/mainHero.js
+++ src/components/heros/mainHero.js
@@ -1,3 +1,14 @@
+/*
+ * Copyright © 2026 Silly Little Games Studio LTD
+ *
+ * This source code is provided for viewing and reference purposes only.
+ * Redistribution, modification, commercial reuse, or use for AI training,
+ * machine learning, automated code-generation systems, or similar purposes
+ * is not permitted without prior written permission.
+ *
+ * See LICENSE.txt for full terms.
+ */
+
 import styles from "./mainHero.module.css";
 
 export const MainHero = ({ takeRef }) => {
