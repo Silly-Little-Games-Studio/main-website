ba45b451d27bc07818fc5c626fa551233ef61c40
Jack Gravil <jackgravil@sillylittlegames.studio>
2026-08-06 15:43:43 +0100

    Finish BlogHero*
    Need to add logic to make webpages from markdown files then pull data from there to auto add the newest 3 blogs to this hero

--- src/components/heros/blog.js
+++ src/components/heros/blog.js
@@ -6,6 +6,11 @@ export const Blog = ({ id }) => {
       <heroSection
         style={{
           backgroundColor: "#170c28",
+          backgroundImage: "url(/Images/BooksWireFrame.png)",
+          backgroundPosition: "right bottom",
+          backgroundRepeat: "no-repeat",
+          minHeight: "600px",
+          maxHeight: "600px",
         }}
       >
         <section id={id} />
@@ -13,6 +18,27 @@ export const Blog = ({ id }) => {
           <h1>Blog</h1>
           <h2>No AI here, just rambling about game design</h2>
         </heroHeading>
+        <div className={styles.articleItem}>
+          <div className={styles.articleDate}>06/08/2026</div>
+          <div className={styles.articleTitle}>There is no articles yet</div>
+          <a href="#" className={styles.articleReadPost}>
+            Read Post
+          </a>
+        </div>
+        <div className={styles.articleItem}>
+          <div className={styles.articleDate}>06/08/2026</div>
+          <div className={styles.articleTitle}>There is no articles yet</div>
+          <a href="#" className={styles.articleReadPost}>
+            Read Post
+          </a>
+        </div>
+        <div className={styles.articleItem}>
+          <div className={styles.articleDate}>06/08/2026</div>
+          <div className={styles.articleTitle}>There is no articles yet</div>
+          <a href="#" className={styles.articleReadPost}>
+            Read Post
+          </a>
+        </div>
       </heroSection>
     </>
   );
